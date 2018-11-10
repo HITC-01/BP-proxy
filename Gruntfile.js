8465d47a811dad069452967dea861dec5d7135f9
Brenden <brenden.pennington@gmail.com>
2018-11-10 14:17:13 -0800

    Update proxy server

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+module.exports = (grunt) => {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    awskey: process.env.AWS_KEY,
+    awssecret: process.env.AWS_SECRET,
+    awsbucket: process.env.AWS_BUCKET,
+    s3: {
+      options: {
+        key: '<%= awskey %>',
+        secret: '<%= awssecret %>',
+        bucket: '<%= awsbucket %>',
+        access: 'public-read',
+        headers: {
+          'Cache-Control': 'max-age=630720000, public',
+          Expires: new Date(Date.now() + 63072000000).toUTCString(),
+        },
+      },
+      dev: {
+        upload: [
+          {
+            src: 'public/style.css',
+            dest: 'style.css',
+          },
+        ],
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-s3');
+
+  grunt.registerTask('default', ['s3']);
+};
--- server/server.js
+++ server/server.js
@@ -1,125 +1,34 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-// const port = process.env.PORT || 3000;
-// const proxy = require('express-http-proxy');
-// // const httpProxy = require('http-proxy');
-
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use('/', proxy('http://localhost:3002/'));
-// app.use('/', proxy('http://localhost:3003/'));
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
-
-// app.listen(port, () => {
-//   console.log(`server running at: http://localhost:${port}`);
-// });
-
-
-// const express = require('express');
-// const proxy = require('express-http-proxy');
-// const path = require('path');
-// const httpProxy = require('http-proxy');
-// const parser = require('body-parser');
-// const cors = require('cors');
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// app.get("/", function(req, res) {
-//   res.redirect("/related");
-// })
-//glory - '/user/songs/:songid'
-// app.get("/", function (req, res) {
-//   res.redirect("/user/");
-// })
-// app.use('/songs/:songId', proxy('http://localhost:3001/'));
-// app.use('/', proxy('http://localhost:3001/'));
-// app.use('/', proxy('http://localhost:3002'));
-// app.use('/related/songs/:songid/related',
-//   proxy({ target: "http://localhost:3002", changeOrigin: true })
-// );
-// app.use('/', proxy('http://localhost:3003/song/:id'));
-// app.use('/', proxy('http://localhost:3004/player/sc'));
-
-// const apiProxy = httpProxy.createProxyServer();
-
-// const user = 'http://localhost:3001',
-//   related = 'http://localhost:3002',
-//   comment = 'http://localhost:3003',
-//   player = 'http://localhost:3004';
-// app.use(cors());
-
-// app.set("port", port);
-
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express());
-// app.use(parser.json());
-
-// app.get("/", function(req, res) {
-//   res.redirect("/");
-// })
-
-// app.use(
-//   '/related', 
-//   proxy({ target: 'http://localhost:3002/', changeOrigin: true})
-// );
-
-// app.use(
-//   '/songs/:songId',
-//   proxy({ target: 'http://localhost:3001/', changeOrigin: true })
-// );
-
-// app.get('/', function(req, res) {
-//   const songPath = path.join(__dirname, "public");
-//   res.sendFile(songPath);
-// })
-// app.use('/', proxy('http://localhost:3004/'));
-// app.use('/songs/:songId', proxy('http://localhost:3001/songs/:songId'));
-// app.use('/', proxy('http://localhost:3004/player/sc'));
-// app.all('/', (req, res) => {
-//   apiProxy.web(req, res, { target: related });
-// });
-
-// app.all('/api/songs/:songid/', (req, res) => {
-//   apiProxy.web(req, res, { target: related });
-// });
-
-
 const express = require('express');
 const app = express();
 const httpProxy = require('http-proxy');
 const port = 3000;
 
 
+
 const apiProxy = httpProxy.createProxyServer();
-const serverOne = 'http://localhost:3001';  
-const serverThree = 'http://localhost:3002'; 
-const serverTwo = 'http://localhost:3003';  
-const serverFour = 'http://localhost:3004'
+const serverOne = 'http://ec2-54-183-128-92.us-west-1.compute.amazonaws.com/';  
+const serverTwo = 'http://ec2-54-215-217-201.us-west-1.compute.amazonaws.com/';  
+const serverThree = 'http://ec2-54-185-4-172.us-west-2.compute.amazonaws.com/'; 
+const serverFour = 'http://ec2-18-222-200-123.us-east-2.compute.amazonaws.com/'
 
 app.use('/songs/:songId', express.static('./public'));
-//any request with user --> glory server
+app.use('/', express.static('./public'));
+
+app.all("/player/*", function (req, res) {
+  apiProxy.web(req, res, { target: serverFour });
+});
+
 app.all("/user/*", function (req, res) {
-  console.log('redirecting to Server1');
   apiProxy.web(req, res, { target: serverOne });
 });
 
 app.all("/comments/*", function (req, res) {
-  console.log('redirecting to Server2');
-  apiProxy.web(req, res, { target: serverTwo });
-});
-
-app.all("/related/*", function (req, res) {
-  console.log('redirecting to Server3');
   apiProxy.web(req, res, { target: serverThree });
 });
 
-app.all("/player/*", function (req, res) {
-  console.log('redirecting to Server4');
-  apiProxy.web(req, res, { target: serverFour });
+app.all("/related/*", function (req, res) {
+  apiProxy.web(req, res, { target: serverTwo });
 });
+
 
 app.listen(port, () => console.log('Server listening on port ' + port));
